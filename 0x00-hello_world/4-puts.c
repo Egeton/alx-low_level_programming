081e28197e4bb39dffbe0fa77ac140100ae9bef3
Egeton <egentinwamaka@yahoo.com>
2023-02-16 20:10:12 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - print aline of code using puts
+/**
+ * main - print  line of code using puts
  *
  * Return 0;
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+	return(0);
 }
