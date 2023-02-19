a98ddfd777c51216911374cd8a3293c6dd3478ba
Egeton <egentinwamaka@yahoo.com>
2023-02-18 22:09:06 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints the alphabet in lowercase\n
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
