ab19fe145a19749fb328c804ccd02898f961bb39
Egeton <egentinwamaka@yahoo.com>
2023-02-21 16:40:38 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,8 @@
  */
 
 int main(void)
-('=') putchar
 {
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -16,4 +16,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-{
+}
